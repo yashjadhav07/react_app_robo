7aef621f37776bb834a3dae6170a97c634d0e3d9
Yash Jadhav <53169635+yashjadhav07@users.noreply.github.com>
2020-07-20 14:44:46 +0530

    Updates

---
+++ precache-manifest.fe3d9288b74a6233f0abe589525b3400.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af13aca578f99ac09f00db600709db0e",
+    "url": "/react_app_robo/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/react_app_robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a8f5341de999d21319f4",
+    "url": "/react_app_robo/static/css/main.3ccf682f.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/react_app_robo/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_app_robo/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f5341de999d21319f4",
+    "url": "/react_app_robo/static/js/main.bfae754e.chunk.js"
+  },
+  {
+    "revision": "81bea4aedffe5d5fc036",
+    "url": "/react_app_robo/static/js/runtime-main.87d7ef06.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react_app_robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
